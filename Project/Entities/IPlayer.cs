63d97060ddc5551c62e994296437addadd1a29b2
RobertWetzler <rwetzler779@gmail.com>
2021-09-20 11:47:10 -0400

    Created entity and player interfaces

---
+++ Project/Entities/IPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project
+{
+    public enum MoveDir
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    interface IPlayer: IEntity
+    {
+        void Move(MoveDir dir);
+    }
+}
