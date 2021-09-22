a4ef52c53faa949147710d33517256095f98c24f
Maggie Feng <2013mfstar@gmail.com>
2021-09-22 16:57:52 -0400

    add block classes and display blocks on screen

---
+++ Project/Sprites/BlockSprites/BrickBlockSprite.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Sprites.BlockSprites
+{
+    public class BrickBlockSprite : IBlock
+    {
+        private Texture2D blockSpriteSheet;
+        private int sheetRows;
+        private int sheetColumns;
+        private int spriteRow;
+        private int spriteColumn;
+
+        public BrickBlockSprite(Texture2D blockSpriteSheet, int sheetRows, int sheetColumns)
+        {
+            this.blockSpriteSheet = blockSpriteSheet;
+            this.sheetRows = sheetRows;
+            this.sheetColumns = sheetColumns;
+            spriteRow = 2;
+            spriteColumn = 0;
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            int width = blockSpriteSheet.Width / sheetColumns;
+            int height = blockSpriteSheet.Height / sheetRows + 1;
+            int scale = 4;
+
+            Rectangle source = new Rectangle(spriteColumn * width, spriteRow * height, width, height - 1);
+            Rectangle dest = new Rectangle((int)position.X, (int)position.Y, width * scale, height * scale);
+            spriteBatch.Draw(blockSpriteSheet, dest, source, Color.White);
+        }
+    }
+}
--- Project/Sprites/BlockSprites/IBlock.cs
+++ Project/Sprites/BlockSprites/IBlock.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Project.Sprites.BlockSprites
 {
     public interface IBlock
     {
-        void Draw(SpriteBatch spriteBatch);
+        void Draw(SpriteBatch spriteBatch, Vector2 position);
     }
 }
