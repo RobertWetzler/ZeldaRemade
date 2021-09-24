7ca915d211272180c53d035c7c696bc2c358259d
RobertWetzler <rwetzler779@gmail.com>
2021-09-23 23:25:52 -0400

    Added direction parameters to SpriteFactory methods

---
+++ Project/Entities/LinkStates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Entities
+{
+    public enum Facing
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    public enum Move
+    {
+        Moving,
+        Idle
+    }
+    public enum LinkColor
+    {
+        Green,
+        Red,
+        Blue
+    }
+}
