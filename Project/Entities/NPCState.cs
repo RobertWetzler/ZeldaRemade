d3969f551f84c87fee2549c612a1c0a73b346cc5
EdwinP1998 <90634053+EdwinP1998@users.noreply.github.com>
2021-09-24 17:03:52 -0400

    I started the creation of NPC state and sprite factory

---
+++ Project/Entities/NPCState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Entities
+{
+    public enum Facing
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    public enum Move
+    {
+        Moving,
+        idle
+    }
+}
