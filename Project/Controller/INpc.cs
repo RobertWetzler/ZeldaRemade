4919f282b41438d04e931feef37ef4d997db94f5
jacobbatt <jacobjojo30@gmail.com>
2021-09-26 18:10:59 -0400

    NPC

---
+++ Project/Controller/INpc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CrossPlatformDesktopProject
+{
+    public interface INpc
+    {
+        void Draw(SpriteBatch spriteBatch);
+        void Update();
+    }
+}
---
+++ Project/Controller/INpcState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CrossPlatformDesktopProject
+{
+    public interface INpcState
+    {
+        void Draw(SpriteBatch spriteBatch, float xPos, float yPos);
+        void Update();
+    }
+}
