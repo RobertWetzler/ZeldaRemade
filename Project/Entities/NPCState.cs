fa65c4a98a6bbf0bcb0a6f8b0c00e1a87c34cd67
EdwinP1998 <90634053+EdwinP1998@users.noreply.github.com>
2021-09-26 15:26:37 -0400

    NPCState machine 2nd commit

--- Project/Entities/NPCState.cs
+++ Project/Entities/NPCState.cs
@@ -14,6 +14,43 @@ namespace Project.Entities
     public enum Move
     {
         Moving,
-        idle
+        Idle
     }
+    class NPCStateMachine
+    {
+        private Facing facing;
+        private Move move;
+
+        public NPCStateMachine(Facing facing, Move move)
+        {
+            this.facing = facing;
+            this.move = move;
+            
+        }
+        public void MoveUp()
+        {
+            this.facing = Facing.Up;
+            this.move = Move.Moving;
+        }
+        public void MoveDown()
+        {
+            this.facing = Facing.Down;
+            this.move = Move.Moving;
+        }
+        public void MoveLeft()
+        {
+            this.facing = Facing.Left;
+            this.move = Move.Moving;
+        }
+        public void MoveRight()
+        {
+            this.facing = Facing.Right;
+            this.move = Move.Moving;
+        }
+        public void StopMoving()
+        {
+            this.move = Move.Idle;
+        }
+    }
+
 }
