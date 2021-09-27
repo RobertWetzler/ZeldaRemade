068826681c9a443b0bdd787531ba960b1cfaaf56
jacobbatt <jacobjojo30@gmail.com>
2021-09-27 09:37:56 -0400

    Update INPC.cs

--- Project/Controller/INPC.cs
+++ Project/Controller/INPC.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CrossPlatformDesktopProject.CollisionHandler;
+using Project.CollisionHandler;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
