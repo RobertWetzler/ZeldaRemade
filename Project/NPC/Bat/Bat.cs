74a86c77c4f9c94c1dcba5c659a4239a47770f5e
Jacob Batt <jacobjojo30@gmail.com>
2021-09-27 11:10:03 -0400

    Add Bat classes

---
+++ Project/NPC/Bat/Bat.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Project.NPC.Bat
+{
+    class Bat : INPC
+    {
+        public INPCState currentState;
+        public float xPos, yPos;
+
+        public Bat()
+        {
+            currentState = new BatWalkEast(this);
+            xPos = 400;
+            yPos = 100;
+        }
+
+        public void Update()
+        {
+            currentState.Update();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            currentState.Draw(spriteBatch, xPos, yPos);
+        }
+    }
+
+}
