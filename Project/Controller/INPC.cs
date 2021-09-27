6694148b23c53a23cb9de3edc7c1f9622b37e42e
jacobbatt <jacobjojo30@gmail.com>
2021-09-27 09:49:16 -0400

    Create INPC.cs

---
+++ Project/Controller/INPC.cs
@@ -0,0 +1,19 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project
+{
+    public interface INPC : ICollisionHandler
+    {
+        void Draw(SpriteBatch spriteBatch);
+        void Update();
+        void TakeDamage();
+        void ChangeDirection();
+    }
+}
