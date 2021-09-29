fa2133d665fb63f7125ec8772e8f6a66f0f0430e
RobertWetzler <rwetzler779@gmail.com>
2021-09-29 18:16:28 -0400

    Fix sprite blurriness

--- Project/Game1.cs
+++ Project/Game1.cs
@@ -79,7 +79,7 @@ namespace Project
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            _spriteBatch.Begin(samplerState:SamplerState.PointClamp);
+            _spriteBatch.Begin(samplerState:SamplerState.PointClamp); // PointClamp fixes sprite blurriness
 
             blocks[CurrentBlockSpriteIndex].Draw(_spriteBatch, new Vector2(200, 100));
 
