d9cf629dc8f9bb05b773f7d9dd841f8a4190fcbe
Jacob Batt <jacobjojo30@gmail.com>
2021-09-30 13:40:15 -0400

    Update NPCSpriteFactory.cs

--- Project/Factory/NPCSpriteFactory.cs
+++ Project/Factory/NPCSpriteFactory.cs
@@ -12,8 +12,7 @@ namespace Project.Factory
     {
         private Texture2D bossDragonSpriteSheet;
         private Texture2D dinosaurLeftRightSpriteSheet;
-        private Texture2D dinosaurUpDownSpriteSheet;
-        private Texture2D batSpriteSheet;
+        private Texture2D dinosaurUpDownSpriteSheet; 
         private Texture2D gelSpriteSheet;
         private Texture2D skeletonSpriteSheet;
         private Texture2D goriyaSpriteSheet;
@@ -41,7 +40,6 @@ namespace Project.Factory
             bossDragonSpriteSheet = content.Load<Texture2D>("EnemySprites/boss-dragon");
             dinosaurLeftRightSpriteSheet = content.Load<Texture2D>("EnemySprites/dinosaur-front-back");
             dinosaurUpDownSpriteSheet = content.Load<Texture2D>("EnemySprites/dinosaur-left-right");
-            batSpriteSheet = content.Load<Texture2D>("EnemySprites/enemy-bat");
             gelSpriteSheet= content.Load<Texture2D>("EnemySprites/enemy-gel");
             skeletonSpriteSheet = content.Load<Texture2D>("EnemySprites/enemy-skeleton");
             goriyaSpriteSheet = content.Load<Texture2D>("EnemySprites/enemy-goriya-walking");
