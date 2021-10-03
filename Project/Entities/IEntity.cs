54500e44196c660a43f942d661c0d2a1028c7876
Robert Wetzler <42687232+RobertWetzler@users.noreply.github.com>
2021-10-02 22:15:23 -0400

    Rwetzler player movement (#55)
    * Created state machine and sprite selector (WIP)
    
    * Updates to keyboard controller, sprite selector, and state machine
    
    * Update to SpriteSelector
    
    * Movement with block placeholder
    
    * Basic movement without animation working
    
    * Updated keyboard controller to only execute commands once per key press
    
    * Improved keyboard controller
    
    * Formatting, dont instantiate defaultCommand to null
    
    * Removed TODO comment

--- Project/Entities/IEntity.cs
+++ Project/Entities/IEntity.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Project
 {
-    interface IEntity
+    public interface IEntity
     {
         public void TakeDamage(int damage);
 
         //TODO: add parameter: Item item
         public void UseItem();
+        void Update(Rectangle windowBounds, GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch, GameTime gameTime);
     }
 }
