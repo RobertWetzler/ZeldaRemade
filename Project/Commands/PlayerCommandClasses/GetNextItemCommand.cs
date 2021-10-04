84bde2fb22ae05fcd6db33f72153b8e24aca9717
JenaFogarty <59618911+fogartyj98@users.noreply.github.com>
2021-10-03 22:34:03 -0400

    Forgot to add triforce

--- Project/Commands/PlayerCommandClasses/GetNextItemCommand.cs
+++ Project/Commands/PlayerCommandClasses/GetNextItemCommand.cs
@@ -16,7 +16,7 @@ namespace Project
         {
             game.CurrentItemSpriteIndex++;
 
-            if(game.CurrentItemSpriteIndex > 24)
+            if(game.CurrentItemSpriteIndex > 25)
             {
                 game.CurrentItemSpriteIndex = 0;
             }
