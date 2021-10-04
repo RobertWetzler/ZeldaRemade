b0d1795119889ad6035d4ab87e03b0c894a43b62
JenaFogarty <59618911+fogartyj98@users.noreply.github.com>
2021-10-03 20:48:55 -0400

    I cleaned up the sprite sheet, and added animations to the sprites that needed it via Maggie's link *Added RupeeSprite, HeartSprite, spriteSheet_items.
    If possible merge ASAP, because this is a very minimal but clean change

--- Project/Sprites/ItemSprites/RupeeSprite.cs
+++ Project/Sprites/ItemSprites/RupeeSprite.cs
@@ -3,32 +3,36 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Project.Sprites.ItemSprites
 {
-    class HealthSprite : IItemSprite
+    class RupeeSprite : IItemSprite
     {
         private int sheetRows;
         private int sheetColumns;
         private int spriteRow;
-       
-
+        //private int spriteColumn;
         private int frame;
+
+
+
 
         private Texture2D spriteSheet;
         //Texture, Rows, Columns
-        public HealthSprite(Texture2D spriteSheet, int sheetRows, int sheetColumns)
+        public RupeeSprite(Texture2D spriteSheet, int sheetRows, int sheetColumns)
         {
             this.spriteSheet = spriteSheet;
             this.sheetColumns = sheetColumns;
             this.sheetRows = sheetRows;
 
             spriteRow = 0;
-            
+
+
+
 
         }
 
         public void Draw(SpriteBatch spriteBatch, Vector2 position)
         {
             int width = spriteSheet.Width / sheetColumns;
-            int height = spriteSheet.Height / sheetRows + 3;
+            int height = spriteSheet.Height / sheetRows;
             int scale = 2;
 
             Rectangle spriteRectangle = new Rectangle(frame * width, spriteRow * height, width, height);
@@ -39,7 +43,9 @@ namespace Project.Sprites.ItemSprites
 
         public void Update(GameTime gameTime)
         {
-            frame = (int)(gameTime.TotalGameTime.TotalSeconds * 3) % 3;
+            frame = (int)(gameTime.TotalGameTime.TotalSeconds * 2) % 2;
+
+
         }
     }
 }
