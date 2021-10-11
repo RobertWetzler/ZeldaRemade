88d200986a91ee6d5133ab3c20b673c795013c81
Maggie Feng <2013mfstar@gmail.com>
2021-10-11 18:47:04 -0400

    refactor block sprites to have less duplicate code

--- Project/Sprites/BlockSprites/BlockSprite.cs
+++ Project/Sprites/BlockSprites/BlockSprite.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Project.Sprites.BlockSprites
 {
-    public class BrickBlockSprite : IBlockSprite
+    class BlockSprite : IBlockSprite
     {
         private Texture2D blockSpriteSheet;
         private int sheetRows;
@@ -11,23 +14,20 @@ namespace Project.Sprites.BlockSprites
         private int spriteRow;
         private int spriteColumn;
 
-        public BrickBlockSprite(Texture2D blockSpriteSheet, int sheetRows, int sheetColumns)
+        public BlockSprite(Texture2D blockSpriteSheet, int sheetRows, int sheetColumns, int blockRow, int blockCol)
         {
             this.blockSpriteSheet = blockSpriteSheet;
             this.sheetRows = sheetRows;
             this.sheetColumns = sheetColumns;
-            spriteRow = 2;
-            spriteColumn = 0;
-
+            this.spriteRow = blockRow;
+            this.spriteColumn = blockCol;
         }
-
         public void Draw(SpriteBatch spriteBatch, Vector2 position)
         {
             int width = blockSpriteSheet.Width / sheetColumns;
-            int height = blockSpriteSheet.Height / sheetRows + 1;
+            int height = blockSpriteSheet.Height / sheetRows;
             int scale = 4;
-
-            Rectangle source = new Rectangle(spriteColumn * width, spriteRow * height, width, height - 1);
+            Rectangle source = new Rectangle(spriteColumn * width, spriteRow * height, width, height);
             Rectangle dest = new Rectangle((int)position.X, (int)position.Y, width * scale, height * scale);
             spriteBatch.Draw(blockSpriteSheet, dest, source, Color.White);
         }
