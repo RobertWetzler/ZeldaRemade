62b51c7d1faf6a6a64bab673e4557cf7fb6d56d9
JenaFogarty <59618911+fogartyj98@users.noreply.github.com>
2021-10-11 22:28:16 -0400

    Item Object Classes created

---
+++ Project/Items/BlueCandle.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Project.Factory;
+using Project.Sprites.ItemSprites;
+
+namespace Project.Items
+{
+    class BlueCandle : IItems
+    {
+
+        private IItemSprite sprite;
+
+
+        public BlueCandle()
+        {
+
+            sprite = ItemSpriteFactory.Instance.CreateItemSprite(0, 3);
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            sprite.Draw(spriteBatch, position);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            sprite.Update(gameTime);
+        }
+    }
+}
