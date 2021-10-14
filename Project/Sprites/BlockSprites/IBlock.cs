decd92a8b0b33761395086c39b534a16bc955bcf
EdwinP1998 <90634053+EdwinP1998@users.noreply.github.com>
2021-10-14 14:27:21 -0400

    Added IBlock interface that draws and updates methods. Update right noe is empty since most blocks don't change their position. May delete IBlockSprite later as it doesn't have update. Changed BlockSprite.cs to connect to the IBlock interface.

---
+++ Project/Sprites/BlockSprites/IBlock.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Sprites.BlockSprites
+{
+    public interface IBlock
+    {
+        //drawing sprite and position of it on screen
+        void Draw(SpriteBatch spriteBatch, Vector2 position);
+        
+        // most blocks do not move
+        void Update();
+    }
+}
