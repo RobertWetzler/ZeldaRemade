c1e263fe4d4f62dbc210611c66f7baceaee78e9c
jacobbatt <jacobjojo30@gmail.com>
2021-10-24 14:44:06 -0400

    Update Project/Collision/CollisionHandlers/EnemyPlayerCollisionHandler.cs
    Co-authored-by: Robert Wetzler <42687232+RobertWetzler@users.noreply.github.com>

--- Project/Collision/CollisionHandlers/EnemyPlayerCollisionHandler.cs
+++ Project/Collision/CollisionHandlers/EnemyPlayerCollisionHandler.cs
@@ -6,7 +6,7 @@ namespace Project.Collision.CollisionHandlers
 {
     class EnemyPlayerCollisionHandler : ICollisionHandler
     {
-        public void HandleCollision(ICollidable enemyCollidable, ICollidable enemy, CollisionSide side)
+        public void HandleCollision(ICollidable weapon, ICollidable enemyCollidable, CollisionSide side)
         {
             IEnemy enemy1 = enemyCollidable as IEnemy;
             enemy1.TakeDamage(1); //TODO: use a damage variable from enemy
