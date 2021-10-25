967482cf991a1721887e327f8c63b8dc95ced0bb
Maggie Feng <2013mfstar@gmail.com>
2021-10-25 17:33:55 -0400

    trap move state

---
+++ Project/Enemies/Trap/TrapMove.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project
+{
+    class TrapMove : IEnemyState
+    {
+        private Trap trap;
+        private EnemyDirections direction;
+        private Vector2 startPos;
+
+        public TrapMove(Trap trap, EnemyDirections direction)
+        {
+            this.trap = trap;
+            this.direction = direction;
+            startPos = trap.Position;
+        }
+        public void ChangeDirection(EnemyDirections direction)
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            switch (direction)
+            {
+                case EnemyDirections.North:
+                    break;
+                case EnemyDirections.West:
+                    break;
+                case EnemyDirections.South:
+                    break;
+                case EnemyDirections.East:
+                    break;
+            }
+        }
+
+        public void UseWeapon()
+        {
+        }
+    }
+}
