bd2e484ef023db819a2f7483a34aed39d3da190d
Pallithanam <pallithanam.1@coeit.osu.edu>
2021-10-25 12:40:35 -0400

    Made a Font Factory that will message the dungeon hint from the olman class

---
+++ Project/Factory/FontFactory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Factory
+{
+    public class FontFactory
+    {
+        private SpriteFont font;
+       
+
+        private static FontFactory instance = new FontFactory();
+
+        public static FontFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private FontFactory()
+        {
+
+        }
+        public void LoadContent(ContentManager content)
+        {
+            font = content.Load<SpriteFont>("OldMan_Message");
+        }
+
+        public SpriteFont GetOldManMessage()
+        {
+            return font;
+        }
+
+    }
+
+ }
