d5629134cd579ceef3496601a8bc1639526fd37d
Jacob Batt <jacobjojo30@gmail.com>
2021-11-12 11:16:45 -0500

    Update EnemyMovableBlockCollisionHandler.cs

--- Project/Collision/CollisionHandlers/Enemies/EnemyMovableBlockCollisionHandler.cs
+++ Project/Collision/CollisionHandlers/Enemies/EnemyMovableBlockCollisionHandler.cs
@@ -8,7 +8,7 @@ namespace Project.Collision.CollisionHandlers.Enemies
 {
     class EnemyMovableBlockCollisionHandler : ICollisionHandler
     {
-        public void HandleCollision(ICollidable blockCollidable, ICollidable enemy, CollisionSide side)
+        public void HandleCollision(ICollidable enemy, ICollidable blockCollidable, CollisionSide side)
         {
             MovableBlock block = blockCollidable as MovableBlock;
 
