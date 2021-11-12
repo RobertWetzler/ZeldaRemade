64977c30214cd9f628644cc6e84e6208da707fb5
Maggie Feng <2013mfstar@gmail.com>
2021-11-12 10:24:07 -0500

    create hud classes and interface

---
+++ Project/HUD/IHUD.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.HUD
+{
+    public interface IHUD
+    {
+        void Update();
+        void Draw(SpriteBatch spriteBatch)
+    }
+}
---
+++ Project/HUD/SmallHUD.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Project.Projectiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.HUD
+{
+    /**
+     * Describe the part of the HUD with the life bar,
+     * A/B items, coin/key/bomb counts, and blue map
+     */
+    class SmallHUD : IHUD
+    {
+        private int bombs;
+        private int coins;
+        private int keys;
+        private IProjectile aItem;
+        private IProjectile bItem;
+        private bool showMap;
+        private Rectangle playerLocation;
+
+        public SmallHUD()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
