dfcae32c192f3a14e6e8b06dbdc54f7a5a96d6de
RobertWetzler <rwetzler779@gmail.com>
2021-11-16 00:13:23 -0500

    Fix locked door bug

--- Project/Collision/CollisionHandlers/Doors/DoorPlayerCollisionHandler.cs
+++ Project/Collision/CollisionHandlers/Doors/DoorPlayerCollisionHandler.cs
@@ -12,13 +12,13 @@ namespace Project.Collision.CollisionHandlers.Doors
         {
             IDoor door = doorCollidable as IDoor;
             IPlayer player = playerCollidable as IPlayer;
-            if(door.IsClosed)
+            if(door.DoorType == DoorType.KEY_CLOSED && player.Inventory.GetItemCount(ItemType.Key) > 0)
             {
-                if(player.Inventory.GetItemCount(ItemType.Key) > 0)
-                {
                     HandleUnlock(player, door);
-                }
-                (new PlayerBlockCollisionHandler()).HandleCollision(playerCollidable, doorCollidable, CollisionUtils.Opposite(side));
+            }
+            else if (door.IsClosed)
+            { 
+                new PlayerBlockCollisionHandler().HandleCollision(playerCollidable, doorCollidable, CollisionUtils.Opposite(side));
             }
             else if (IsPlayerHittingEdge(player)) {
                 switch (door)
