97bef428918fb4c5b5d8cb50be3717101f1e7ebf
Yuting Yang <yang.5493@buckeyemail.osu.edu>
2021-11-16 17:42:54 -0500

    Fix map bug

--- Project/GameState/ItemSelectionState.cs
+++ Project/GameState/ItemSelectionState.cs
@@ -71,6 +71,9 @@ namespace Project.GameState
             keyboardController.Update();
             itemSelectionScreen.Update(gameTime);
             itemSelector.Update(gameTime);
+            /*
+             * Update posDot's position 
+             */
             switch (RoomManager.Instance.CurrentRoom.RoomID)
             {
                 case 1:
@@ -188,68 +191,65 @@ namespace Project.GameState
 
 
             smallHud.Draw(spriteBatch);
-            if (player.Inventory.GetItemCount(ItemType.Map) > 0)
-            {
                 map.Draw(spriteBatch, new Vector2(165, 440));
                 foreach (var room in game.PassedRoom)
                 {
                     switch (room)
                     {
-                        case 0:
+                        case 1:
                             mapTile1.Draw(spriteBatch, new Vector2(400, 464 + heightOffset), Color.White);
                             break;
-                        case 1:
+                        case 2:
                             mapTile2.Draw(spriteBatch, new Vector2(500, 400 + heightOffset), Color.White);
                             break;
-                        case 2:
+                        case 3:
                             mapTile3.Draw(spriteBatch, new Vector2(500, 464 + heightOffset), Color.White);
                             break;
-                        case 3:
+                        case 4:
                             mapTile4.Draw(spriteBatch, new Vector2(500, 496 + heightOffset), Color.White);
                             break;
-                        case 4:
+                        case 5:
                             mapTile5.Draw(spriteBatch, new Vector2(500, 560 + heightOffset), Color.White);
                             break;
-                        case 5:
+                        case 6:
                             mapTile6.Draw(spriteBatch, new Vector2(532, 400 + heightOffset), Color.White);
                             break;
-                        case 6:
+                        case 7:
                             mapTile7.Draw(spriteBatch, new Vector2(532, 432 + heightOffset), Color.White);
                             break;
-                        case 7:
+                        case 8:
                             mapTile8.Draw(spriteBatch, new Vector2(532, 464 + heightOffset), Color.White);
                             break;
-                        case 8:
+                        case 9:
                             mapTile9.Draw(spriteBatch, new Vector2(532, 496 + heightOffset), Color.White);
                             break;
-                        case 9:
+                        case 10:
                             mapTile10.Draw(spriteBatch, new Vector2(532, 528 + heightOffset), Color.White);
                             break;
-                        case 10:
+                        case 11:
                             mapTile11.Draw(spriteBatch, new Vector2(532, 560 + heightOffset), Color.White);
                             break;
-                        case 11:
+                        case 12:
                             mapTile12.Draw(spriteBatch, new Vector2(564, 464 + heightOffset), Color.White);
                             break;
-                        case 12:
+                        case 13:
                             mapTile13.Draw(spriteBatch, new Vector2(564, 496 + heightOffset), Color.White);
                             break;
-                        case 13:
+                        case 14:
                             mapTile14.Draw(spriteBatch, new Vector2(564, 560 + heightOffset), Color.White);
                             break;
-                        case 14:
+                        case 15:
                             mapTile15.Draw(spriteBatch, new Vector2(596, 432 + heightOffset), Color.White);
                             break;
-                        case 15:
+                        case 16:
                             mapTile16.Draw(spriteBatch, new Vector2(596, 464 + heightOffset), Color.White);
                             break;
-                        case 16:
+                        case 17:
                             mapTile17.Draw(spriteBatch, new Vector2(628, 432 + heightOffset), Color.White);
                             break;
                     }
-                    posDot.Draw(spriteBatch, dotPos, Color.White);
                 }
-            }
+                posDot.Draw(spriteBatch, dotPos, Color.White);
         }
     }
 }
