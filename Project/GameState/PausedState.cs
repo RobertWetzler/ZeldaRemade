7ae706726e6b5ecaffbd170c10f097e1396c6b08
Maggie Feng <2013mfstar@gmail.com>
2021-11-29 22:15:55 -0500

    draw life bar when paused

--- Project/GameState/PausedState.cs
+++ Project/GameState/PausedState.cs
@@ -11,9 +11,8 @@ namespace Project.GameState
     class PausedState : IGameState
     {
         private Game1 game;
-        private KeyboardController keyboardController;
         private IHUD smallHUD;
-        private ISprite spriteBlank, spriteA, spriteD, spriteE, spriteP, spriteS, spriteU;
+        private ISprite spriteA, spriteD, spriteE, spriteP, spriteS, spriteU;
         private List<ISprite> textList = new List<ISprite>();
         private int textLinePosX = 420;
         private int textLinePosY = 500;
@@ -24,7 +23,6 @@ namespace Project.GameState
             this.game = game;
             smallHUD = new SmallHUD(false);
             SoundManager.Instance.backgroundInstance.Pause();
-            spriteBlank = TextSpriteFactory.Instance.CreateBlankSprite();
             spriteA = TextSpriteFactory.Instance.CreateASprite();
             spriteD = TextSpriteFactory.Instance.CreateDSprite();
             spriteE = TextSpriteFactory.Instance.CreateESprite();
@@ -43,6 +41,7 @@ namespace Project.GameState
         public void Update(GameTime gameTime, Rectangle bounds)
         {
             PauseController.controller.Update();
+            smallHUD.Update();
         }
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
