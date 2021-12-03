d3035dcb64df3465c29d2e2f1a29d36fa7b03cf9
Jacob Batt <jacobjojo30@gmail.com>
2021-12-03 11:28:19 -0500

    Bombs do more damage

--- Project/Collision/CollisionHandlers/Enemies/EnemyBombCollisionHandler.cs
+++ Project/Collision/CollisionHandlers/Enemies/EnemyBombCollisionHandler.cs
@@ -7,10 +7,12 @@ namespace Project.Collision.CollisionHandlers.Enemies
         public void HandleCollision(ICollidable enemyCollidable, ICollidable projectileCollidable, CollisionSide side)
         {
 
+            IEnemy enemy = enemyCollidable as IEnemy;
             Bomb bomb = projectileCollidable as Bomb;
 
             if (bomb.IsExploding)
             {
+                enemy.Health.DecreaseHealth(10);
                 new EnemyProjectileCollisionHandler().HandleCollision(enemyCollidable, projectileCollidable, side);
             }
         }
