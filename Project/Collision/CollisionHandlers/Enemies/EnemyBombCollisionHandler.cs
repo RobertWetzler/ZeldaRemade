d11cda6903b3eb7e3d5df85dace6b8f71e3d5595
Jacob Batt <jacobjojo30@gmail.com>
2021-12-04 12:59:36 -0500

    Update EnemyBombCollisionHandler.cs

--- Project/Collision/CollisionHandlers/Enemies/EnemyBombCollisionHandler.cs
+++ Project/Collision/CollisionHandlers/Enemies/EnemyBombCollisionHandler.cs
@@ -12,7 +12,7 @@ namespace Project.Collision.CollisionHandlers.Enemies
 
             if (bomb.IsExploding)
             {
-                enemy.Health.DecreaseHealth(10);
+                enemy.Health.DecreaseHealth(4);
                 new EnemyProjectileCollisionHandler().HandleCollision(enemyCollidable, projectileCollidable, side);
             }
         }
