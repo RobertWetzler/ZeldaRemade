49dd00a526557c8dcd202bb957064b12dafe828a
Yuting Yang <yang.5493@buckeyemail.osu.edu>
2021-12-06 18:28:41 -0500

    Deleted comments

--- Project/Entities/SpinningLink.cs
+++ Project/Entities/SpinningLink.cs
@@ -12,7 +12,7 @@ namespace Project.Entities
     {
         private double totalFlashTime = 1000;
         private double remainingFlashTime;
-        private int timeToChangeDirection = 100; //time to randomly change direction
+        private int timeToChangeDirection = 100; 
         private int changeDirectionCounter = 0;
         private Color color;
         private Facing facing;
@@ -33,8 +33,7 @@ namespace Project.Entities
         public override void Update(Rectangle windowBounds, GameTime gameTime)
         {
             remainingFlashTime -= (int)gameTime.ElapsedGameTime.TotalMilliseconds;
-                // Update color if timer is still running
-                if (remainingFlashTime > 0)
+            if (remainingFlashTime > 0)
             {
                 changeDirectionCounter += gameTime.ElapsedGameTime.Milliseconds;
                 if (changeDirectionCounter > timeToChangeDirection)
@@ -59,101 +58,21 @@ namespace Project.Entities
                             facing = Facing.Down;
                             break;
                     }
-                }
-                
-                //UpdateColor();
+                }               
             }
             else
             {
                 this.decoratedPlayer.Update(windowBounds, gameTime);
             }
-            // If both timers depleted, remove decorator
             if (remainingFlashTime <= 0)
             {
                 RemoveDecorator();
             }
         }
-        /*
-        private void UpdateColor()
-        {
-            List<float> hues = new List<float>() { 140f, 180f, 260f, 340f };
-            double t = totalFlashTime - remainingFlashTime;
-            int i = (int)(t / totalFlashTime * hues.Count * 10) % hues.Count; // cycle through list
-            color = ColorUtils.HSVToRGB(hues[i], 1, 1);
-        }
-        private void UpdateKnockback(GameTime gameTime, Rectangle windowBounds)
-        {
-            int x_dir = 0, y_dir = 0;
-            switch (this.decoratedPlayer.StateMachine.facing)
-            {
-                case Facing.Left:
-                    x_dir = 1;
-                    break;
-                case Facing.Right:
-                    x_dir = -1;
-                    break;
-                case Facing.Up:
-                    y_dir = 1;
-                    break;
-                case Facing.Down:
-                    y_dir = -1;
-                    break;
-            }
-            float newX = this.decoratedPlayer.Position.X + (float)(x_dir * gameTime.ElapsedGameTime.TotalSeconds * knockbackVelocity);
-            float newY = this.decoratedPlayer.Position.Y + (float)(y_dir * gameTime.ElapsedGameTime.TotalSeconds * knockbackVelocity);
-            if (x_dir == 1)
-            {
-                newX = (int)(newX + decoratedPlayer.BoundingBox.Width) < windowBounds.Right ? newX : windowBounds.Right - (decoratedPlayer.BoundingBox.Width);
-            }
-            else if (x_dir == -1)
-            {
-                newX = (int)newX > windowBounds.Left ? newX : windowBounds.Left;
-            }
-            else if (y_dir == 1)
-            {
-                newY = (int)(newY + decoratedPlayer.BoundingBox.Height) < windowBounds.Bottom ? newY : windowBounds.Bottom - (decoratedPlayer.BoundingBox.Height);
-            }
-            else
-            {
-                newY = (int)(newY) > windowBounds.Top ? newY : windowBounds.Top;
-            }
-            this.decoratedPlayer.Position = new Vector2(newX, newY);
 
-        }*/
         public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
             this.decoratedPlayer.Draw(spriteBatch, gameTime, color);
         }
-        /*
-      
-        // If move during knockback, stop knockback (similar to real game)
-        public override void MoveUp()
-        {
-            remainingKnockbackTime = 0;
-            base.MoveUp();
-        }
-        public override void MoveDown()
-        {
-            remainingKnockbackTime = 0;
-            base.MoveDown();
-        }
-        public override void MoveLeft()
-        {
-            remainingKnockbackTime = 0;
-            base.MoveLeft();
-        }
-        public override void MoveRight()
-        {
-            remainingKnockbackTime = 0;
-            base.MoveRight();
-        }
-        // only allow weapon after knockback (change later if not desired behavior)
-        public override void UseWeapon(WeaponTypes weaponType)
-        {
-            if (remainingKnockbackTime <= 0)
-            {
-                base.UseWeapon(weaponType);
-            }
-        }*/
     }
 }
