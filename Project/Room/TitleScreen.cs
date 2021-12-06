f4b1fb2edcf74e398dfaa26d5d6cec5e4b725527
fogartyj7898@gmail.com <fogartyj7898@gmail.com>
2021-12-06 18:05:50 -0500

    Removed some stuff

--- Project/Room/TitleScreen.cs
+++ Project/Room/TitleScreen.cs
@@ -10,16 +10,12 @@ namespace Project
     {
         private IBackgroundSprite sprite;
         private Rectangle bounds;
-        private HardButton hardButton;
-        private EasyButton easyButton;
 
 
         public TitleScreen(GraphicsDeviceManager graphics)
         {
             sprite = BackgroundSpriteFactory.Instance.CreateTitleScreen();
             bounds = new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
-            hardButton = new HardButton();
-            easyButton = new EasyButton();
         }
 
         public void Update(GameTime gameTime)
@@ -30,7 +26,6 @@ namespace Project
         public void Draw(SpriteBatch spriteBatch)
         {
             sprite.Draw(spriteBatch, bounds);
-            easyButton.Draw(spriteBatch);
 
         }
     }
