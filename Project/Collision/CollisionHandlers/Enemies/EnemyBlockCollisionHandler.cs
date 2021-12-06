9ffd455e3e8c106bbc68530f1b4cfa45b7b47687
Maggie Feng <2013mfstar@gmail.com>
2021-12-06 18:07:24 -0500

    trying to fix goriya bug

--- Project/Collision/CollisionHandlers/Enemies/EnemyBlockCollisionHandler.cs
+++ Project/Collision/CollisionHandlers/Enemies/EnemyBlockCollisionHandler.cs
@@ -7,27 +7,25 @@ namespace Project.Collision.CollisionHandlers
         public void HandleCollision(ICollidable enemyCollidable, ICollidable block, CollisionSide side)
         {
             IEnemy enemy = enemyCollidable as IEnemy;
-            if (!(enemy is Goriya && ((Goriya)enemy).CurrentState is GoriyaUseItem) && !(enemy is Trap))
+            switch (side)
             {
-                switch (side)
-                {
-                    case CollisionSide.Up:
-                        //Collided with top, move down
-                        enemy.ChangeDirection(EnemyDirections.South);
-                        break;
-                    case CollisionSide.Down:
-                        //Collided with bottom, move up
-                        enemy.ChangeDirection(EnemyDirections.North);
-                        break;
-                    case CollisionSide.Left:
-                        enemy.ChangeDirection(EnemyDirections.East);
-                        break;
-                    case CollisionSide.Right:
-                        enemy.ChangeDirection(EnemyDirections.West);
-                        break;
-                }
+                case CollisionSide.Up:
+                    //Collided with top, move down
+                    enemy.ChangeDirection(EnemyDirections.South);
+                    break;
+                case CollisionSide.Down:
+                    //Collided with bottom, move up
+                    enemy.ChangeDirection(EnemyDirections.North);
+                    break;
+                case CollisionSide.Left:
+                    enemy.ChangeDirection(EnemyDirections.East);
+                    break;
+                case CollisionSide.Right:
+                    enemy.ChangeDirection(EnemyDirections.West);
+                    break;
             }
-            
+
+
         }
     }
 }
