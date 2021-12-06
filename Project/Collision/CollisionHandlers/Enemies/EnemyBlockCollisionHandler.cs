48e9ea4a9a599d967bf5d91bb8b7571542a052ef
Maggie Feng <2013mfstar@gmail.com>
2021-12-06 13:35:18 -0500

    make enemy trap between blocks a bit better

--- Project/Collision/CollisionHandlers/Enemies/EnemyBlockCollisionHandler.cs
+++ Project/Collision/CollisionHandlers/Enemies/EnemyBlockCollisionHandler.cs
@@ -7,32 +7,25 @@ namespace Project.Collision.CollisionHandlers
         public void HandleCollision(ICollidable enemyCollidable, ICollidable block, CollisionSide side)
         {
             IEnemy enemy = enemyCollidable as IEnemy;
-            int dx = 0;
-            int dy = 0;
-            if (!(enemy is Trap))
+            if (!(enemy is Goriya && ((Goriya)enemy).CurrentState is GoriyaUseItem) && !(enemy is Trap))
             {
                 switch (side)
                 {
                     case CollisionSide.Up:
                         //Collided with top, move down
-                        dy = block.BoundingBox.Bottom - enemy.BoundingBox.Top;
                         enemy.ChangeDirection(EnemyDirections.South);
                         break;
                     case CollisionSide.Down:
                         //Collided with bottom, move up
-                        dy = block.BoundingBox.Top - enemy.BoundingBox.Bottom;
                         enemy.ChangeDirection(EnemyDirections.North);
                         break;
                     case CollisionSide.Left:
-                        dx = block.BoundingBox.Right - enemy.BoundingBox.Left;
                         enemy.ChangeDirection(EnemyDirections.East);
                         break;
                     case CollisionSide.Right:
-                        dx = block.BoundingBox.Left - enemy.BoundingBox.Right;
                         enemy.ChangeDirection(EnemyDirections.West);
                         break;
                 }
-                enemy.Position = new Vector2(enemy.Position.X + dx, enemy.Position.Y + dy);
             }
             
         }
