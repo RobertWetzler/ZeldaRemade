3f17f7630cd93ced3b569b132b66a40cf2baf420
Jacob Batt <jacobjojo30@gmail.com>
2021-12-06 11:02:08 -0500

    Add Dinosaur Movements

---
+++ Project/Enemies/Dinosaur/DinosaurWalkNorth.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Project.Factory;
+
+namespace Project
+{
+    class DinosaurWalkNorth : IEnemyState
+    {
+
+        private Dinosaur dinosaur;
+
+        public DinosaurWalkNorth(Dinosaur dinosaur)
+        {
+            this.dinosaur = dinosaur;
+            this.dinosaur.EnemySprite = EnemySpriteFactory.Instance.CreateDinosaurLeftRightSprite();
+        }
+
+        public void ChangeDirection(EnemyDirections direction)
+        {
+            switch (direction)
+            {
+                case EnemyDirections.North:
+                    dinosaur.SetState(new DinosaurWalkNorth(dinosaur));
+                    break;
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            dinosaur.Position = new Vector2((float)(gameTime.ElapsedGameTime.TotalSeconds * dinosaur.Velocity) + dinosaur.Position.X,
+                                            dinosaur.Position.Y);
+        }
+
+        public void UseWeapon()
+        {
+
+        }
+    }
+}
---
+++ Project/Enemies/Dinosaur/DinosaurWalkSouth.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Project.Factory;
+
+namespace Project
+{
+    class DinosaurWalkSouth : IEnemyState
+    {
+
+        private Dinosaur dinosaur;
+
+        public DinosaurWalkSouth(Dinosaur dinosaur)
+        {
+            this.dinosaur = dinosaur;
+            this.dinosaur.EnemySprite = EnemySpriteFactory.Instance.CreateDinosaurLeftRightSprite();
+        }
+
+        public void ChangeDirection(EnemyDirections direction)
+        {
+            switch (direction)
+            {
+                case EnemyDirections.South:
+                    dinosaur.SetState(new DinosaurWalkSouth(dinosaur));
+                    break;
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            dinosaur.Position = new Vector2((float)(gameTime.ElapsedGameTime.TotalSeconds * dinosaur.Velocity) + dinosaur.Position.X,
+                                            dinosaur.Position.Y);
+        }
+
+        public void UseWeapon()
+        {
+
+        }
+    }
+}
